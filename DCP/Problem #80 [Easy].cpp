752cd290965893c119f5b3afc3869de09fb56f8a
vedesh95 <50790991+vedesh95@users.noreply.github.com>
2020-03-20 00:55:23 +0530

    wrote pseudocode

--- DCP/Problem #80 [Easy].cpp
+++ DCP/Problem #80 [Easy].cpp
@@ -1,5 +1,37 @@
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Google.
+
+Given the root of a binary tree, return a deepest node. For example, in the following tree, return d.
+
+    a
+   / \
+  b   c
+ /
+d
+
+
+
 1.The idea here is to find the height of the given tree and then print the node at the bottom-most level.
 O(n)
 
 2.The idea is to do Inorder traversal of given binary tree. While doing Inorder traversal, we pass level of current node also. 
 We keep track of maximum level seen so far and value of deepest node seen so far.
+
+int  util(Node* root,int level,int &maxlevel,int &res)
+{
+  util(root->left,level++,maxlevel,res);
+  if(level>maxlevel)
+  {
+    res=root->data;maxlevel=level;
+  }
+  
+  util(root->right,level++,maxlevel,res);
+    
+}
+int deppest(Node* root)
+{
+  int res=-1,level=0,maxlevel=-1;
+  util(root,level,maxlevel,res);
+  return res;
+}
