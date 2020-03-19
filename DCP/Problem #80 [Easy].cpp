9a1eede52f6e8c0dea14f1aef030578a42c0fe9e
vedesh95 <50790991+vedesh95@users.noreply.github.com>
2020-03-20 00:38:37 +0530

    Create Problem #80 [Easy].cpp

---
+++ DCP/Problem #80 [Easy].cpp
@@ -0,0 +1,5 @@
+1.The idea here is to find the height of the given tree and then print the node at the bottom-most level.
+O(n)
+
+2.The idea is to do Inorder traversal of given binary tree. While doing Inorder traversal, we pass level of current node also. 
+We keep track of maximum level seen so far and value of deepest node seen so far.
